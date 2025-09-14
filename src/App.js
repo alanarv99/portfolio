de2501af1b08879ad90316eb043645518be5829e
alanarv99 <147183094+alanarv99@users.noreply.github.com>
2025-09-14 18:40:44 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header>
+        <h1>Alana Vazquez</h1>
+      </header>
+      <footer>
+        <h6>
+          This project was coded by Alana Vazquez and is open-sourced on{" "}
+          <a href="https://github.com/alanarv99/portfolio" target="_blank">
+            Github
+          </a>
+        </h6>
+      </footer>
     </div>
   );
 }
